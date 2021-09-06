e16790a6309ec40dcb710f8ad4b238b921587235
Jim Hessin <jhessin@gmail.com>
2021-09-05 18:54:17 -0600

    copied source from lesson notes

--- www/js/game.js
+++ www/js/game.js
@@ -1,53 +1,54 @@
-const image = new Image(),
-  takePhotoButton = document.querySelector("#takePhotoButton");
-
-let constraints, imageCapture, mediaStream, video;
-
-const init = () => {
-  video = document.querySelector("video");
-
-  navigator.mediaDevices
-    .enumerateDevices()
-    .then(getStream)
-    .catch((error) => console.log("enumerateDevices() error", error));
-  takePhotoButton.addEventListener("click", getPicture);
-};
-
-// Get a video stream from the camera
-const getStream = () => {
-  if (mediaStream) {
-    mediaStream.getTracks().forEach((track) => track.stop());
-  }
-
-  constraints = {
-    video: {
-      width: 720,
-      height: 720,
-    },
+// js/game.js
+{
+  const image = new Image(),
+    takePhotoButton = document.querySelector(".takePhoto");
+  let constraints, imageCapture, mediaStream, video;
+  const init = () => {
+    video = document.querySelector("video");
+    navigator.mediaDevices
+      .enumerateDevices()
+      .catch((error) => console.log("enumerateDevices() error", error))
+      .then(getStream);
+    takePhotoButton.addEventListener("click", getPicture);
   };
 
-  navigator.mediaDevices
-    .getUserMedia(constraints)
-    .then(gotStream)
-    .catch((error) => console.log("getUserMedia() error", error));
-};
-
-// Display the stream from the camera, and then
-// create an ImageCapture object, using video from the stream
-const gotStream = (stream) => {
-  mediaStream = stream;
-  video.srcObject = stream;
-  imageCapture = new ImageCapture(stream.getVideoTracks()[0]);
-};
-
-// Take the picture
-const getPicture = () => {
-  imageCapture
-    .takePhoto()
-    .then((img) => {
-      image.src = URL.createObjectURL(img);
-      image.addEventListener("load", () => createImagePieces(image));
-      setInterval(() => checkDistance(), 1000);
-    })
-    .catch((error) => console.log("takePhoto() error", error));
-};
+  // Get a video stream from the camera
+  const getStream = () => {
+    if (mediaStream) {
+      mediaStream.getTracks().forEach((track) => track.stop());
+    }
+    constraints = {
+      video: {
+        width: 720,
+        height: 720,
+      },
+    };
+    navigator.mediaDevices
+      .getUserMedia(constraints)
+      .catch((error) => {
+        console.log("getUserMedia error", error);
+      })
+      .then(gotStream);
+  };
+  // Display the stream from the camera, and then create an ImageCapture object, using video from the stream
+  const gotStream = (stream) => {
+    mediaStream = stream;
+    video.srcObject = stream;
+    imageCapture = new ImageCapture(stream.getVideoTracks()[0]);
+  };
+  //Take the picture
+  const getPicture = () => {
+    imageCapture
+      .takePhoto()
+      .then((img) => {
+        image.src = URL.createObjectURL(img);
+        image.addEventListener("load", () => createImagePieces(image));
+        setInterval(() => checkDistance(), 1000);
+      })
+      .catch((error) => {
+        console.log("takePhoto() error", error);
+      });
+  };
+  const createImagePieces = (image) => {};
+  const checkDistance = () => {};
+}
